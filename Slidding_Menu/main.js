348d6e90fec939e56c358bd88d5d189284c3a802
elizabethtellez <tel21004@byui.edu>
2023-04-06 16:10:42 -0700

    final evaluation

---
+++ Slidding_Menu/main.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    var updateElement = document.getElementById("list");
+    updateElement.classList.toggle("open");
+  }   
+  
+  function hamSwitch(x) {
+    x.classList.toggle("ham-switch");
+  }
